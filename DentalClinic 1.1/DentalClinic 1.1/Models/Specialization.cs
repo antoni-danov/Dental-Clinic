c4016946692169125959b5c8c9ac8f0d14efe44f
Antoni Danov <antoni.danov@gmail.com>
2020-11-15 17:18:35 +0100

    Constructor to Specialization

--- DentalClinic 1.1/DentalClinic 1.1/Models/Specialization.cs
+++ DentalClinic 1.1/DentalClinic 1.1/Models/Specialization.cs
@@ -7,6 +7,10 @@ namespace DentalClinic_1._1.Models
 {
     public class Specialization
     {
+        public Specialization()
+        {
+            this.Users = new HashSet<ApplicationUser>();
+        }
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<ApplicationUser> Users { get; set; }
