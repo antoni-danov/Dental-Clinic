0fad3db9b1509d45134790d5b39c968535bf7c17
Antoni <59748393+antoni-danov@users.noreply.github.com>
2020-12-08 22:54:40 +0100

    Update DentistsController.cs

--- DentalClinic 1.1/DentalClinic 1.1/Controllers/DentistsController.cs
+++ DentalClinic 1.1/DentalClinic 1.1/Controllers/DentistsController.cs
@@ -26,26 +26,7 @@ namespace DentalClinic_1._1.Controllers
         }
         public IActionResult Patients()
         {
-            List<AllPatientsViewModel> listOfPatients = new List<AllPatientsViewModel>();
-            var patients = await userManager.GetUsersInRoleAsync("Patient");
-
-            foreach (var patient in patients)
-            {
-
-                var users = new AllPatientsViewModel
-                {
-                    FirstName = patient.Firstname,
-                    LastName = patient.Lastname,
-                    PhoneNumber = patient.PhoneNumber,
-                    Email = patient.Email,
-                    Id = patient.Id
-                };
-
-
-                listOfPatients.Add(users);
-            }
-
-            return View(listOfPatients);
+                       return View();
         }
         public async Task<IActionResult> Appointments()
         {
