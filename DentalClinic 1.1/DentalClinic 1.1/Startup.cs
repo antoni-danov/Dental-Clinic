cef819dfa75cc18e690a3d52c314d8156d0d1b2e
Antoni Danov <antoni.danov@gmail.com>
2020-12-18 20:10:57 +0100

    Update version of framework

--- DentalClinic 1.1/DentalClinic 1.1/Startup.cs
+++ DentalClinic 1.1/DentalClinic 1.1/Startup.cs
@@ -69,6 +69,8 @@ namespace DentalClinic_1._1
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
+                app.UseStatusCodePagesWithReExecute("/Home/Error", "statusCode={0}");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
