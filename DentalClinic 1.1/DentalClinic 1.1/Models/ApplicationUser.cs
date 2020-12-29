74ce9651c742f72c7b4bf21a88a1c9b2885d9d41
Antoni Danov <antoni.danov@gmail.com>
2020-12-29 11:39:15 +0100

    change

--- DentalClinic 1.1/DentalClinic 1.1/Models/ApplicationUser.cs
+++ DentalClinic 1.1/DentalClinic 1.1/Models/ApplicationUser.cs
@@ -16,22 +16,14 @@ namespace DentalClinic_1._1.Models
         {
             Appointments = new HashSet<Appointment>();
         }
-        [StringLength(20, MinimumLength = 3)]
-        [RegularExpression(RegexFirstName)]
-        [Required(ErrorMessage = "Enter valid first name.")]
+    
         public string Firstname { get; set; }
 
-        [StringLength(20, MinimumLength = 3)]
-        [RegularExpression(RegexFirstName)]
-        [Required(ErrorMessage = "Enter valid last name.")]
         public string Lastname { get; set; }
 
-        [Required]
-        [DataType(DataType.Date)]
         public DateTime Birthdate { get; set; }
-        [Required]
         public string Address { get; set; }
-        [Required(ErrorMessage = "Enter valid town name.")]
+       
         public string Town { get; set; }
         public string Specialty { get; set; }
         public ICollection<Appointment> Appointments { get; set; }
